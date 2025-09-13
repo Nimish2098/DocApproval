e4dd2bc1b05c1218d1c6216146aac248f5eaf07b
Nimish2098 <raonimish23@gmail.com>
2025-09-14 00:11:30 +0530

    Approval Line

---
+++ src/main/java/com/Project/DocApproval/service/ApprovalService.java
@@ -0,0 +1,11 @@
+package com.Project.DocApproval.service;
+
+import com.Project.DocApproval.repository.ApprovalRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApprovalService {
+
+    ApprovalRepository approvalRepository;
+
+}
